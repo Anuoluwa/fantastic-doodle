b2e7d36072778f48356b5e75e6b48baa63126f73
Anuoluwa Apiti <anuoluwa.apiti@gmail.com>
2020-04-20 21:28:10 +0100

    add challenge questions

---
+++ py/OOP/oop.py
@@ -0,0 +1,22 @@
+"""
+OOP = modeling object 
+"""
+
+""" 
+Challenge
+Use the planning strategy and process demonstrated above to design a parking lot. This is a common OOP interview question and the time you spend thoughtfully thinking through this problem will be helpful.
+
+As you design your solution, and think through all of the necessary objects, attributes, methods, and relationships, keep the following in mind:
+
+Payments
+What methods are allowed?
+Where do customers pay?
+Capacity
+How does the system respond when the lot is full?
+How does the system know when the lot is full?
+Vehicles
+Is capacity allocated differently depending on what type of vehicle enters the lot? How so?
+Pricing
+Are there different rates for different times of day? How will this be handled?
+Is there a discount for purchasing a longer total time?
+"""
