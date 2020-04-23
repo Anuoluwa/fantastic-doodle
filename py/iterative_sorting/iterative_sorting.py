bf70adcf87c9d2ea077006fd4a1e93a5d3406863
Anuoluwa Apiti <anuoluwa.apiti@gmail.com>
2020-04-23 23:05:03 +0100

    add iterative sorting files and selection sort solutinds

---
+++ py/iterative_sorting/iterative_sorting.py
@@ -0,0 +1,18 @@
+arr = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
+# TO-DO: Complete the selection_sort() function below
+def selection_sort( arr ):
+    # loop through n-1 elements
+    for i in range(0, len(arr) - 1):
+        cur_index = i
+        smallest_index = cur_index
+        # TO-DO: find next smallest element
+        # (hint, can do in 3 loc)
+        for j in range(cur_index + 1, len(arr)):
+            if arr[j] < arr[smallest_index]:
+                smallest_index = j
+
+        arr[smallest_index], arr[cur_index] = arr[cur_index], arr[smallest_index]
+
+        # TO-DO: swap
+
+    return arr
