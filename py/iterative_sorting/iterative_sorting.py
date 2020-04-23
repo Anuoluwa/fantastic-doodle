23f7eaf752cf0d63d8f991be17349ede71a5cc2b
Anuoluwa Apiti <anuoluwa.apiti@gmail.com>
2020-04-23 23:07:48 +0100

    add selection sort definitions from wiki

--- py/iterative_sorting/iterative_sorting.py
+++ py/iterative_sorting/iterative_sorting.py
@@ -1,4 +1,12 @@
 arr = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
+
+"""
+    selection sort is an in-place comparison sorting algorithm. It has an O(n2) time complexity, 
+    which makes it inefficient on large lists, and generally performs worse than the similar insertion sort. 
+    Selection sort is noted for its simplicity and has performance advantages over more complicated algorithms in certain situations, 
+    particularly where auxiliary memory is limited.
+"""
+
 # TO-DO: Complete the selection_sort() function below
 def selection_sort( arr ):
     # loop through n-1 elements
