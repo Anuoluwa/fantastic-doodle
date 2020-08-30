fa94fef4074e764f69d725d6975cd2c975dcb8e9
Anuoluwa Apiti <anuoluwa.apiti@gmail.com>
2020-08-30 13:28:09 +0100

    new solutions for search and replace of substring

---
+++ code_challenge/techdevguide/recursion-subsgring.py
@@ -0,0 +1,14 @@
+string = input("Enter a word: ")
+substring = input("Please enter the substring you wish to find: ")
+replacement = input("Please enter a string to replace the give substring: ")
+
+def search_and_replace(string, substring, replacement):
+    if not string:  #if the string is empty
+        return ""
+    elif string[:len(substring)] == substring:
+        return replacement + search_and_replace(string[len(substring):],
+                                                substring, replacement)
+    else:
+        return string[0] + search_and_replace(string[1:], substring,
+                                              replacement)
+print(search_and_replace(string, substring, replacement))
